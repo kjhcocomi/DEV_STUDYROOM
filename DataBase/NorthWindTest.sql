d3631a8afd9b93717c115e1392edf5b1ea214fab
kjhcocomi <981028_@naver.com>
2022-03-16 13:09:37 +0900

    .

--- DataBase/NorthWindTest.sql
+++ DataBase/NorthWindTest.sql
@@ -595,3 +595,85 @@ FROM TestOrders AS o
 	-- HashTable 만드는 비용을 무시하면 안됨 (수행빈도가 많으면 -> 인덱스를 만드는게 효율적)
 -- 3) 랜덤 엑세스 위주로 수행되지 않는다.
 -- 4) 데이터가 적은 쪽을 HashTable로 만드는 것이 유리하다.
+
+
+
+-- Sorting(정렬) 을 줄이자!
+USE BaseballData;
+
+-- O(NLogN) -> DB는 데이터가 어마어마하게 많다.
+-- 너무 용량이 커서 가용 메모리로 커버가 안되면 -> 디스크까지 찾아간다 (엄청느림)
+-- Sorting이 언제 일어나는지 파악하고 있어야 함
+
+-- Sorting이 일어날 때
+-- 1) SORT MERGE JOIN
+	-- 원인) 알고리즘 특성상 Merge하지 전에 Sort를 해야 함
+-- 2) ORDER BY
+-- 3) GROUP BY
+-- 4) DISTINCT
+-- 5) UNION
+-- 6) RANKING WINDOWS FUNCTION
+-- 7) MIN MAX
+
+
+-- 1) 생략
+
+-- 2) ORDER BY
+-- 원인) ORDER BY 순서로 정렬을 해야 하니까 Sort
+SELECT *
+FROM players
+ORDER BY college;
+
+-- 인덱스 있으면 Sorting 안함 
+SELECT *
+FROM batting
+ORDER BY playerID, yearID;
+
+-- 3) GROUP BY
+-- 원인) 집계를 하기 위해
+SELECT college, COUNT(college)
+FROM players
+WHERE college LIKE 'C%'
+GROUP BY college
+
+-- 인덱스 있으면 Sorting 안함 
+SELECT playerID, COUNT(playerID)
+FROM players
+WHERE playerID LIKE 'C%'
+GROUP BY playerID
+
+-- 4) DISTINCT
+-- 원인) 중복 제거
+SELECT DISTINCT college
+FROM players
+WHERE college LIKE 'C%'
+
+-- 5) UNION
+-- 원인) 중복 제거
+SELECT college
+FROM players
+WHERE college LIKE 'B%'
+UNION
+SELECT college
+FROM players
+WHERE college LIKE 'C%'
+
+-- 중복 제거가 필요 없을 경우
+SELECT college
+FROM players
+WHERE college LIKE 'B%'
+UNION ALL
+SELECT college
+FROM players
+WHERE college LIKE 'C%'
+
+-- 6) RANKING WINDOWS FUNCTION
+-- 원인) 집계를 하기 위해
+SELECT ROW_NUMBER() OVER (ORDER BY college)
+FROM players;
+
+-- 인덱스가 있는 경우
+SELECT ROW_NUMBER() OVER (ORDER BY PlayerID)
+FROM players;
+
+-- INDEX를 잘 활용하면 Sorting을 굳이 하지 않아도 됨
