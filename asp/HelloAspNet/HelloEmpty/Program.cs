1361eedb27de6d2bc228ffe58c9d4c6d7372ee5a
kjhcocomi <981028_@naver.com>
2022-03-17 22:33:23 +0900

    2022.03.17 ASP.NET Core 둘러보기

---
+++ asp/HelloAspNet/HelloEmpty/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloEmpty
+{
+    // M (Model)       데이터     (원자재)
+    // V (View)        UI         (인테리어)
+    // C (Controller)  Controller (액션)
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
